9e9ab0069b902cc8a72d7e8fb8e5c8217abb8f0a
wilson-kerr <wrkerr@asu.edu>
2026-02-23 23:54:39 -0700

    Update lqr_controller.py
    optimal values tested

--- assignments/cart_pole_optimal_control/cart_pole_optimal_control/lqr_controller.py
+++ assignments/cart_pole_optimal_control/cart_pole_optimal_control/lqr_controller.py
@@ -35,8 +35,8 @@ class CartPoleLQRController(Node):
         ])
         
         # LQR cost matrices
-        self.Q = np.diag([1.0, 1.0, 1.0, 1.0])  # State cost
-        self.R = np.array([[1.0]])  # Control cost
+        self.Q = np.diag([10.0, 10.0, 50.0, 50.0])  # State cost
+        self.R = np.array([[0.05]])  # Control cost
         
         # Compute LQR gain matrix
         self.K = self.compute_lqr_gain()
